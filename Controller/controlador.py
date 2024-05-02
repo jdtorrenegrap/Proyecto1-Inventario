c5aa307b1127678083c0a2d1d707afd9133bb150
Jesus Torrenegra <jdtorrenegrap@ul.edu.co>
2024-05-02 08:42:25 -0500

    async await

--- Controller/controlador.py
+++ Controller/controlador.py
@@ -1,52 +1,67 @@
 import sys
 sys.path.insert(0, '/Proyectos/Proyecto-1/View') 
-sys.path.insert(0, '/Proyectos/Proyecto-1/Model') 
+sys.path.insert(0, '/Proyectos/Proyecto-1/Model')
 from data import BaseDatos
 from view import Vista_Opciones
+import asyncio
 
 
 
 class Controlador:
+    """Controlador para la aplicación de gestión de productos."""
+
     def __init__(self):
+        """Inicializa el controlador con una base de datos."""
         self.model = BaseDatos('data.db')
-        self.model.crear_tabla()
+        self.model.crear_tabla()    
 
     def iniciar_app(self):
+        """Inicia la aplicación."""
         while True:
             Vista_Opciones.menu()
             opcion = input ("[Opción]->")
-            self.procesar_opc(opcion)
+            if opcion == '5':
+                print("¡Gracias por usar nuestro sistema!")
+                break
+            asyncio.run(self.procesar_opcion(opcion))
 
-    def procesar_opc(self, opcion):
+    async def procesar_opcion(self, opcion):
+        """Procesa la opción seleccionada por el usuario."""
         if opcion =='1':
-            self.agregar_producto()
+           await self.agregar_producto()
         elif opcion == '2':
             self.ver_producto()
         elif opcion == '3':
             self.eliminar_producto()
         elif opcion =='4':
             self.iniciar_chat()
-        elif opcion == '5':
-              print("¡Gracias por usar nuestro sistema!")
-              exit()
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
-    
-    def agregar_producto(self):
-        codigo,nombre,cantidad = Vista_Opciones.agregar_producto()
-        self.model.agregar_productos(codigo,nombre,cantidad)
-        Vista_Opciones.mostrar_mensaje("[PRODUCTO AGREGADO.]")
+
+    async def agregar_producto(self):
+        """Agrega un producto a la base de datos."""
+        n= int(input("Productos a agregar ->"))
+        for i in range(n):
+            codigo, nombre, cantidad = Vista_Opciones.agregar_producto()
+            await asyncio.sleep(1)
+            Vista_Opciones.mostrar_mensaje("[REALIZANDO PROCESO..]")
+            self.model.agregar_productos(codigo, nombre, cantidad)
+            Vista_Opciones.mostrar_mensaje("[PRODUCTO AGREGADO.]")
     
     def ver_producto(self):
-         productos = self.model.listar_productos()
-         Vista_Opciones.mostrar_productos(productos)
+        """Muestra los productos en la base de datos."""
+        productos = self.model.listar_productos()
+        Vista_Opciones.mostrar_productos(productos)
 
     def eliminar_producto(self):
+        """Elimina un producto de la base de datos."""
         codigo = Vista_Opciones.eliminar_producto()
         producto_eliminado = self.model.eliminar_productos(codigo)
         if producto_eliminado:
             Vista_Opciones.mostrar_mensaje("[PRODUCTO ELIMINADO]")
         else:
             Vista_Opciones.mostrar_mensaje("[PRODUCTO NO ENCONTRADO.]")
+
     def iniciar_chat(self):
+        """Inicia el chat."""
         Vista_Opciones.mostrar_mensaje("¡Estamos trabajando en una nueva función! ¡Pronto estará disponible!")
